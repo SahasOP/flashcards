18bad859d9b511d68f8a48d8bca766034000e37b
SahasOP <140003995+SahasOP@users.noreply.github.com>
2024-08-12 22:46:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
+app.use(cors({
+    origin:'https://flashcards-two-red.vercel.app',
+}));
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
