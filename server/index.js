3de108ea35d49c63a5c725a58b8af0e521967a25
SahasOP <140003995+SahasOP@users.noreply.github.com>
2024-08-12 22:50:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
-const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors({
-    origin:'*',
-}));
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
